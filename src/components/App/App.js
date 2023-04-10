6f59583387823a09eb0503feb182d2b31ff6edde
Jesus Borjas <jesusbor006@gmail.com>
2023-04-10 10:33:07 -0500

    Add Tricks component to store Card component and have default state value

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,33 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import Tricks from "./Tricks/Tricks";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tricks: [
+        {
+          id: 1,
+          name: "treflip",
+        },
+        {
+          id: 2,
+          name: "heelflip",
+        },
+        {
+          id: 3,
+          name: "frontside 50-50, backside 180 out",
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
+        <Tricks allTricks />
       </div>
     );
   }
---
+++ src/components/App/Tricks/Tricks.js
@@ -0,0 +1,13 @@
+import Card from "../Card/Card";
+import "./Tricks.css";
+
+const Tricks = () => {
+  return (
+    <div>
+      <h1>Ideas go here</h1>
+      <Card />
+    </div>
+  );
+};
+
+export default Tricks;
